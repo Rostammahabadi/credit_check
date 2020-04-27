55204933e4826ca6ead51e77285a3a6ba0f5973f
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-27 11:38:52 -1000

    update

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,6 +1,39 @@
+require 'pry'
 card_number = "5541808923795240"
 
+
 # Your Luhn Algorithm Here
+summed_digits = []
+sum = 0
+digits = card_number.split("")
+digits.map!.with_index do |digit, index|
+  if index.even?
+    digit.to_i * 2
+  else
+    digit.to_i
+  end
+end
+
+digits.each_with_index do |digit, index|
+  if index % 2 == 0
+    if digit > 9
+      summed_digits << digit.digits.sum
+    elsif digit < 9
+      summed_digits << digit
+    end
+  elsif index % 2 != 0
+    summed_digits << digit
+  end
+end
+sum_digits = summed_digits.sum
+multiplied = sum_digits*9
+result = multiplied % 10
+
+if result == 0
+  puts "The number #{card_number} is valid!"
+else
+  puts "The number #{card_number} is invalid!"
+end
 
 # Output
 ## If it is valid, print "The number [card number] is valid!"
